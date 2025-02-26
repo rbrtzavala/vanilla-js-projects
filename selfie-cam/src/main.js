d9440ccc17f9979415ca2b9ca59703c1210ef6b4
Robert Zavala <rbrtzavala@gmail.com>
2025-02-26 16:24:18 -0700

    refactor main async function for cross browser compatability

--- selfie-cam/src/main.js
+++ selfie-cam/src/main.js
@@ -1,13 +1,13 @@
 import './style.css'
 import { getVideo, drawVideo } from './camera'
 
-const video = await getVideo()
-const canvas = document.querySelector('canvas')
-const button = document.getElementById('camera')
 
-button.addEventListener('click', () => {
-    console.log('drawing')
-    drawVideo(video, canvas)
-})
-
-await video.play()
+(async function run() {
+    const video = await getVideo()
+    const canvas = document.querySelector('canvas')
+    const button = document.getElementById('camera')
+    
+    button.addEventListener('click', () => {
+        drawVideo(video, canvas)
+    })
+})()
