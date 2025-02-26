d711c920d4f6b3ddea91451a38b793e459f48ac1
Robert Zavala <rbrtzavala@gmail.com>
2025-02-26 16:13:38 -0700

    refactor modules, add click event to access user camers

--- selfie-cam/src/camera.js
+++ selfie-cam/src/camera.js
@@ -1,11 +1,22 @@
-export async function getVideo() {
+export const getVideo = async () => {
     const avStream = await navigator.mediaDevices.getUserMedia({
-        // audio: true,
         video: true
     })
 
     const video = document.createElement('video')
-    video.srcObject = avStream
+    try {
+        video.srcObject = avStream
+    } catch(err) {
+        video.src = window.URL.createObjectURL(avStream)
+    }
+
     await video.play()
     return video
+}
+
+export function drawVideo(video, canvas) {
+    const context = canvas.getContext('2d')
+    canvas.width = video.videoWidth
+    canvas.height = canvas.width
+    context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
 }
--- selfie-cam/src/main.js
+++ selfie-cam/src/main.js
@@ -1,26 +1,13 @@
 import './style.css'
-import { getVideo } from './camera'
+import { getVideo, drawVideo } from './camera'
+
 const video = await getVideo()
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+const canvas = document.querySelector('canvas')
+const button = document.getElementById('camera')
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+button.addEventListener('click', () => {
+    console.log('drawing')
+    drawVideo(video, canvas)
+})
 
-// setupCounter(document.querySelector('#counter'))
+await video.play()
